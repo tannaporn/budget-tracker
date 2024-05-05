bfd98c0fd073e34cea9d4a76c8203224a1ed9c05
Tannaporn Raksasri <68152912+tannaporn@users.noreply.github.com>
2024-05-05 20:35:07 +0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import  RootProvider from "@/components/providers/RootProviders";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Budget Tracker",
+  description: "CodeWithFern",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+        <html lang="en" className="dark" style={{colorScheme:"dark"}}>
+          <body className={inter.className}>
+           <RootProvider>{children}</RootProvider> 
+            </body>
+        </html>
+    </ClerkProvider>
+  );
+}
